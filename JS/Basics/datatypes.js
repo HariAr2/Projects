6e6c93c48fca13f7f0f13333f4972c8c08fabea8
Hari Aravindh <127916422+HariAr2@users.noreply.github.com>
2023-10-22 21:00:22 +0530

    Add files via upload

---
+++ JS/Basics/datatypes.js
@@ -0,0 +1,18 @@
+const a=5;
+const b="HELLO";
+const c=true;
+const d=[1,2,3,4];
+let e;
+let f = BigInt("123456789012345678901234567890");
+const g={name:"NAME",age:"AGE"};
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+console.log(typeof(f));
+console.log(typeof(g));
+
+/**          OUTPUT
+
+ */
