c0bda9f28c88087dfd2dc7204ebf24de56a96fa0
HariAr2 <ha7014@srmist.edu.in>
2023-12-02 16:47:18 +0530

    added SideBar

--- animevault/src/App.js
+++ animevault/src/App.js
@@ -1,9 +1,19 @@
-import Header from "./components/Header";
 
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 function App() {
+  const [animeList, SetAnimeList] = useState([]);
+  const [topAnime, SetTopAnime] = useState([]);
+  const [search, SetSearch] = useState("");
+
+
   return (
     <div className="App">
       <Header/>
+      <div className="content-wrap">
+        <Sidebar topAnime = {topAnime}/>
+      </div>
     </div>
   );
 }
---
+++ animevault/src/components/Sidebar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <aside>
+        <nav>
+            <h3>Top Anime</h3>
+            <a href="#" target="_blank" rel='noreferrer' >Attack on Titan</a>
+            <a href="#" target="_blank" rel='noreferrer' >Sword Art online</a>
+            <a href="#" target="_blank" rel='noreferrer' >Demon Slayer</a>
+            
+        </nav>
+    </aside>
+  )
+}
+
+export default Sidebar
